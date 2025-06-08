e74376d5f546f2ad2d83e5b4151521e192b5da36
icnu <172075965+icnu@users.noreply.github.com>
2025-06-08 15:58:24 +0530

    async poc

--- src/enclave/src/main.rs
+++ src/enclave/src/main.rs
@@ -1,42 +1,29 @@
-use std::{os::fd::{AsRawFd, OwnedFd}, sync::Arc};
-use nix::sys::socket::{connect, socket, AddressFamily, SockFlag, SockType, VsockAddr};
-use ntk_common::{relay, ENCLAVE_IP, HOST_CID, HOST_IP, HOST_PORT, TUN_NETMASK};
-use tun::{AbstractDevice, Configuration, Device};
+use std::{thread, time};
+use ntk_common::{relay, tun::{create_tun_device, DeviceType}, vsock::vsock_connect, HOST_PORT};
+use tun::AbstractDevice;
 
-fn create_tun_device() -> Device {
-    let mut config = Configuration::default();
-    config
-        .address(ENCLAVE_IP)
-        .destination(HOST_IP)
-        .netmask(TUN_NETMASK)
-        .up();
-
-    let tun_dev = tun::create(&config).unwrap();
-    tun_dev
-}
-
-fn vsock_connect() -> Result<OwnedFd, ()> {
-    let sockaddr = VsockAddr::new(HOST_CID, HOST_PORT);
-    let socket = socket(
-        AddressFamily::Vsock,
-        SockType::Stream,
-        SockFlag::empty(),
-        None
-    ).unwrap();
-
-    match connect(socket.as_raw_fd(), &sockaddr) {
-        Ok(_) => Ok(socket),
-        Err(_) => Err(())
-    }
-}
-
-fn main() {
-    let tun_dev = create_tun_device();
-    let tun_dev = Arc::new(tun_dev);
+fn main() -> Result<(), String> {
+    let mut tun_dev = create_tun_device(DeviceType::Enclave)?;
     println!("TUN device {} connected.", tun_dev.tun_name().unwrap());
 
-    let vsock = vsock_connect().unwrap();
-    println!("Vsock connected.");
+    loop {
+        let connect_result = vsock_connect(HOST_PORT);
+        match connect_result {
+            Err(e) => {
+                println!("Encountered error while connecting: {:?}", e);
+            }
+            
+            Ok(vsock) => {
+                println!("Vsock connected.");
+                
+                let mut vsock = vsock;
+                relay(&mut vsock, &mut tun_dev);
+            }
+        }
 
-    relay(vsock.as_raw_fd(), tun_dev);
+        println!("Sleeping 5 secs before retrying.");
+        thread::sleep(time::Duration::from_secs(5));
+    }
+
+    Ok(())
 }
--- src/host/src/main.rs
+++ src/host/src/main.rs
@@ -1,49 +1,22 @@
-use std::{os::fd::{AsRawFd, OwnedFd}, sync::Arc};
+use ntk_common::{relay, tun::{create_tun_device, DeviceType}, vsock::vsock_listen, HOST_PORT};
+use tun::AbstractDevice;
 
-use nix::{libc::VMADDR_CID_ANY, sys::socket::{accept, bind, listen, socket, AddressFamily, Backlog, SockFlag, SockType, VsockAddr}};
-use ntk_common::{relay, ENCLAVE_IP, HOST_IP, HOST_PORT, TUN_NETMASK};
-use tun::{Configuration, Device, AbstractDevice};
-
-fn create_tun_device() -> Device {
-    let mut config = Configuration::default();
-    config
-        .address(HOST_IP)
-        .destination(ENCLAVE_IP)
-        .netmask(TUN_NETMASK)
-        .up();
-
-    let tun_dev = tun::create(&config).unwrap();
-    tun_dev
-}
-
-fn listen_vsock() -> Result<OwnedFd, ()> {
-    let socket = socket(
-        AddressFamily::Vsock,
-        SockType::Stream,
-        SockFlag::empty(),
-        None
-    ).unwrap();
-
-    let sockaddr = VsockAddr::new(VMADDR_CID_ANY, HOST_PORT);
-
-    bind(socket.as_raw_fd(), &sockaddr).unwrap();
-    listen(&socket, Backlog::new(128).unwrap()).unwrap();
-
-    Ok(socket)
-}
-
-fn main() {
-    let tun_dev = create_tun_device();
-    let tun_dev = Arc::new(tun_dev);
+fn main() -> Result<(), String> {
+    let mut tun_dev = create_tun_device( DeviceType::Host )?;
     println!("TUN device {} connected.", tun_dev.tun_name().unwrap());
 
-    let listener = listen_vsock().unwrap();
-    
-    loop {
-        let tun_dev = tun_dev.clone();
-        let vsock = accept(listener.as_raw_fd()).unwrap();
-        println!("Vsock connected.");
+    let listener = vsock_listen( HOST_PORT )?;
+    for vsock in listener.incoming() {
+        match vsock {
+            Ok(vsock) => {
+                println!("Vsock connected!");
 
-        relay(vsock, tun_dev);
-    }
+                let mut vsock = vsock;
+                relay(&mut vsock, &mut tun_dev);
+            },
+            Err(e) => return Err(format!("Error encountered in vsock listener, {:?}", e))
+        };
+    };
+
+    Ok(())
 }
